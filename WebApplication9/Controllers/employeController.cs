394bcff2b14eb8c280630b6d835ff95703a805a8
Baselshreem <Baselshreem12@gmail.com>
2022-02-03 13:50:58 +0200

    first commit

---
+++ WebApplication9/Controllers/employeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using employeserver;
+
+namespace WebApplication9.Controllers
+{
+    public class employeController : ApiController
+    {
+
+        public IEnumerable<emplye> Get()
+        {
+
+            using(employedbentety entyte=new employedbentety())
+            {
+
+               return  entyte.emplye.ToList();
+            }
+        }
+    }
+}
---
+++ WebApplication9/Scripts/google.js
@@ -0,0 +1,75 @@
+﻿function getAccssessToken() {
+
+
+    if (location.hash) {
+
+        if (location.hash.split('access_token=')) {
+
+            var accesstoken = location.split('access_token=')[1].split('&')[0];
+            if (accesstoken) {
+                isuserregisterd(accesstoken);
+
+            }
+        }
+    }
+}
+
+
+function isuserregisterd(accesstoken) {
+
+    $.ajax({
+
+        url: '/api/Account/UserInfo',
+         
+
+        method: 'GET',
+
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': 'Bearer ' + accesstoken
+
+        },
+
+        success: function (response) {
+
+            if (response.hasRegistered) {
+
+                localStorage.setItem('access_token', accesstoken);
+                localStorage.setItem('username', response.Email)
+            }
+            else {
+
+                signupExternaluser(accesstoken);
+            }
+        }
+
+
+    });
+
+}
+
+
+function signupExternaluser(accesstoken) {
+
+    $.ajax({
+
+        url: '/api/Account/RegisterExternal',
+
+
+        method: 'POST',
+     
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': 'Bearer ' + accesstoken
+
+        },
+
+        success: function (response) {
+            window.location.href = "/api/Account/ExternalLogin?provider=Google&response_type=token&client_id=self&redirect_uri=https%3A%2F%2Flocalhost%3A44301%2Flogin.html%2F";
+
+        }
+
+
+    });
+
+}
